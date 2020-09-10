aba0aa8393e70e8488e889af1c807e89d4cf2dba
Ariel Razon <arielrazon@Ariels-MacBook-Pro.local>
2020-09-09 21:15:50 -0700

    initial file structure

---
+++ public/script.js
@@ -0,0 +1,14 @@
+/*
+must:
+add rows with a column for the task, and an X out button
+
+color code things by priority
+
+add drop-down in the submission pane for priority
+
+add submission pane section
+
+host on a database all the information
+*/
+
+
